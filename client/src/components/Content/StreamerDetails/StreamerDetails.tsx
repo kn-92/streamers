b594d804ae6fa23863bb4476ea9273240273a90e
Kamil_Niemiec <kamil_niemiec@o2.pl>
2023-06-27 12:03:07 +0200

    feat: navigating between details page and home page add

--- client/src/components/Content/StreamerDetails/StreamerDetails.tsx
+++ client/src/components/Content/StreamerDetails/StreamerDetails.tsx
@@ -1,7 +1,33 @@
 import "./StreamerDetails.scss";
 
+import photo from "../../../images/streamer-photo.jpg";
+
+import { useNavigate } from "react-router-dom";
+
 const StreamerDetails = () => {
-  return <div>StreamerDetails</div>;
+  const navigate = useNavigate();
+  const handleBackToHomepage = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="details-container">
+      <div onClick={handleBackToHomepage} className="back">
+        {" "}
+        --Back to homepage
+      </div>
+      <div className="details-box">
+        <div className="image-box">
+          <img className="image" src={photo} alt="streamer-name" />
+        </div>
+        <div className="info-box">
+          <p>Coco</p>
+          <p>Best streamer in the world!!!!!!</p>
+          <p>YouTube</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StreamerDetails;
